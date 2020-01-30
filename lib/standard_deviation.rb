f47bbf49cd4b2b38ceb30562f207d60347fc9712
Ana Perez Santos <a.jperez@yahoo.com>
2020-01-30 13:33:56 -0700

    Add all integers

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,26 @@
-ages = [24, 30, 18, 20, 41]
+
+@ages = [24, 30, 18, 20, 41]
 
 # Your code here for calculating the standard deviation
 
+  #add all integers:
+
+    added = @ages.sum
+    p added
+
+      #using a method
+        # def sum
+        #   @ages.sum
+        # end
+        #
+        # p sum
+
 # When you find the standard deviation, print it out
+
+
+
+
+
+
+
+# require "pry"; binding.pry
